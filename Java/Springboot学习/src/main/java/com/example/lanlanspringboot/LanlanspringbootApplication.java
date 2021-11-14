8aec63ec605fc9855caaf5c94d91b62af0b37207
MikeCreken <1171546435@qq.com>
2021-11-14 17:10:45 +0800

    fistr commmit

---
+++ Java/Springboot学习/src/main/java/com/example/lanlanspringboot/LanlanspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.lanlanspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanlanspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanlanspringbootApplication.class, args);
+    }
+
+}
---
+++ Java/Springboot学习/src/test/java/com/example/lanlanspringboot/LanlanspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lanlanspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanlanspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
