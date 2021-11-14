8f7b50797235d4060d14793285bf8499475f9f1f
MikeCreken <1171546435@qq.com>
2021-11-14 21:14:50 +0800

    fix

--- Java/Springboot学习/src/main/java/com/example/lanlanspringboot/LanlanspringbootApplication.java
+++ Java/Springboot学习/src/main/java/com/example/lanlanspringboot/LanlanspringbootApplication.java
@@ -2,7 +2,6 @@ package com.example.lanlanspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class LanlanspringbootApplication {
 
--- Java/Springboot学习/src/test/java/com/example/lanlanspringboot/LanlanspringbootApplicationTests.java
+++ Java/Springboot学习/src/test/java/com/example/lanlanspringboot/LanlanspringbootApplicationTests.java
@@ -2,8 +2,13 @@ package com.example.lanlanspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+
 class LanlanspringbootApplicationTests {
 
     @Test
